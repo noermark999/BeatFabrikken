be92ac6463e8c13cb9a3113e5c57202ca69b6daa
JohnLe <johnle@hotmail.dk>
2023-11-17 09:54:15 +0100

    session

--- service/loginDBFunctions.js
+++ service/loginDBFunctions.js
@@ -22,7 +22,7 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebase_app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const brugere = collection(db, 'Bruger')
 
