8d45338bdc2e397096c7108101ee6c74162d199b
Noermark999 <jakobnoermark@hotmail.com>
2023-11-21 11:52:53 +0100

    SPRINT2-salt

--- service/registreringDBFunctions.js
+++ service/registreringDBFunctions.js
@@ -29,26 +29,45 @@ const brugere = collection(db, 'Bruger')
 
 
 const addUser = async (user) => {
-    user.password = await hashPassword(user.password);
+    user.salt = getSalt();
     console.log(user);
+    const salt = saltStringToUint8Array(user.salt)
+    user.password = await hashPassword(user.password, salt);
     const docRef = await addDoc(brugere, user)
+    console.log(docRef.id);
     return docRef.id
 }
 
 // Funktion til at hashe adgangskoden med SHA-256
-async function hashPassword(password) {
-    // Konverterer adgangskoden til en Uint8Array
+async function hashPassword(password, salt) {
+    // Konverterer adgangskoden og saltet til en Uint8Array
     const encoder = new TextEncoder();
-    const data = encoder.encode(password);
+    const passwordData = encoder.encode(password);
+    const saltedData = new Uint8Array([...passwordData, ...salt]);
 
-    // Hasher adgangskoden med SHA-256
-    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    // Hasher adgangskoden med SHA-256 og saltet
+    const hashBuffer = await crypto.subtle.digest('SHA-256', saltedData);
 
-    // Konverterer det hashede resultat til en hex-streng
+    // Konverterer det hashede resultat til hex-strenge
     const hashArray = Array.from(new Uint8Array(hashBuffer));
     const hashedPassword = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+    const saltString = Array.from(salt).map(byte => byte.toString(16).padStart(2, '0')).join('');
 
+    // Returnerer både det hashede resultat
     return hashedPassword;
 }
 
-export default {addUser, hashPassword}
+function getSalt() {
+    const salt = crypto.getRandomValues(new Uint8Array(16));
+    const saltString = Array.from(salt).map(byte => byte.toString(16).padStart(2, '0')).join('');
+    return saltString;
+}
+
+function saltStringToUint8Array(saltString) {
+    // Deler saltString op i et array af to karakterer og konverterer dem til Uint8Array
+    const uint8Array = new Uint8Array(saltString.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
+
+    return uint8Array;
+}
+
+export default {addUser, hashPassword, saltStringToUint8Array}
