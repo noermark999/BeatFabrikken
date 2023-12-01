a5a0a36f8b83beafcdb424596004baf3cc0fb0dc
JohnLe <johnle@hotmail.dk>
2023-12-01 09:36:58 +0100

    opret hold test

--- test/opretHoldTest.js
+++ test/opretHoldTest.js
@@ -2,7 +2,35 @@ import chai, { expect } from 'chai';
 import supertest from 'supertest';
 import appModule from '../app.js';
 import { getFirestore, collection, doc, getDoc, updateDoc } from 'firebase/firestore';
-import loginDBFunctions from '../service/loginDBFunctions.js';
-import BookingDBFunctions from '../service/BookingDBFunctions.js';
-import administratorDBFunctions.js from '../service/administratorDBFunctions.js';
+import administratorDBFunctions from '../service/administratorDBFunctions.js';
 import e from 'express';
+
+const app = appModule.app;
+const request = supertest(app);
+
+const assert = chai.assert;
+
+describe('test af opret hold', () => {
+    it('burde oprette et hold', async () => {
+      // data for at lave holdet
+      const alder = "13"
+      const holdNavn = "TEST HOLD"
+      const instruktør = "JAKOB"
+      const pris = "100"
+  
+      supertest(app).post('/opretHold').send({hold})
+      
+      // kald på funktionen der opretter bookningen
+      await administratorDBFunctions.addHold(hold);
+  
+      
+      // hent bookningen og tjek om det er korrekt
+      const oprettetHold = await administratorDBFunctions.getHold(holdNavn)
+  
+      // Assertions
+      expect(response.status).to.equal(200);
+      expect(oprettetHold).to.not.be.null;
+      expect(oprettetHold.holdNavn).to.equal(holdNavn);
+    });
+  });
+  
